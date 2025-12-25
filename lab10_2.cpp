1e21ac8388098cf1ab2885fe482ea6f1f9c4a3c0
Mookanda <kaofang0608@gmail.com>
2025-12-25 13:50:36 +0700

    lab10_2.cppcompleted

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,18 +1,27 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main (){
-	ifstream source;
-	ofstream dest;
-	source.open("cheerbook.txt") ;
-	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+int main() {
+    ifstream source;
+    ofstream dest;
+
+    source.open("cheerbook.txt");
+    
+    dest.open("cheerbook_copy.txt");
+    
+    dest << "-------------------- BOOM ---------------------" << endl;
+    
+    string textline;
+    while(getline(source, textline)){
+        dest << textline << endl;
+    }
+    
+    dest << "-------------------- HA!! ---------------------" << endl;
 
     source.close();
     dest.close();
-	return 0;
+    
+    return 0;
 }
