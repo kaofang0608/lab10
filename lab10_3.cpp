f3517edc033399fe615be0f741a468cd5d3d1e19
Mookanda <kaofang0608@gmail.com>
2025-12-25 13:51:22 +0700

    lab10_3.cppcompleted

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include <iomanip>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main(){
+
+    int count = 0;
+    double sum = 0,sum_of_square=0,x=0,sd;
+    
+
+    string score;
+    ifstream source("score.txt");
+
+    while(getline(source,score)){
+        x = atof(score.c_str());
+        sum += x;
+        sum_of_square += x*x;
+        count++;
+    }
+    
+    sd = sqrt((sum_of_square/count)-pow(sum/count,2));
+
+    cout<<"Number of data = "<<count<<"\n";
+    cout << setprecision(3);
+    cout<<"Mean = "<<sum/count<<"\n";
+    cout << "Standard deviation = "<< sd;
+}
